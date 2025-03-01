7a203588f04f937c8729db359255940fe32ef5e7
nta <NoelBansikah.Tandap@adorsys.com>
2025-03-01 14:57:33 +0100

    test2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     println!("This is the test project for FibBot action!");
-    // Any additional test logic can be added here
+    // Any additional test logic can be added here and more functions
 }
